893e39ff3dec02321d4b53ada3836c4b6b289ca8
AntonioCauAt <antoniocaulinatienzar@gmail.com>
2024-09-04 12:58:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,25 +17,26 @@ with open(__location__+'/config.json') as config_json:
     config = json.load(config_json)
     
 
-# == config parameters fw ==
+# == config parameters  ==
 fname_fwd    = config['forward']
 fname_noisecov  = config ['cov']
-subject = 'output'
-
-# == config parameters noisecov ==
 fname = config ['epo']
+
+# == read parameters  ==
 epochs = mne.read_epochs(fname) 
+fwd = mne.read_forward_solution (fname_fwd)
+noisecov = mne.read_cov (fname_noisecov)
 
 # Configuration depending on what we want
 epochs.pick_types(meg=True, eeg=False)
 
 #Fixed forward operator
-#fwd_fixed = mne.convert_forward_solution(fname_fwd, surf_ori=True)
+fwd_fixed = mne.convert_forward_solution(fwd, surf_ori=True)
 
 #Compute the evoked responses for two conditions: faces and scrambled
 evoked = epochs.average()
 info = evoked.info
-inverse_operator = make_inverse_operator (info, fname_fwd, fname_noisecov, loose=0.2, depth=0.8)
+inverse_operator = make_inverse_operator (info, fwd_fixed, noisecov, loose=0.2, depth=0.8)
 
 #Applying inverse operator to our evoked contrast
 method = "dSPM"
